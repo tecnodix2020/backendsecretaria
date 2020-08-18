41b110d0cd0db4135adf6ccc0520dc1fb343328f
Guilherme Ferreira <guifrribeiro@gmail.com>
2020-08-18 00:19:51 -0300

    Add the STATIC_ROOT path

--- smartdeskbackend/settings.py
+++ smartdeskbackend/settings.py
@@ -9,12 +9,16 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 import urllib
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
+
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 
 
 # Quick-start development settings - unsuitable for production
