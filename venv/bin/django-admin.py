985d73171055b9eab6bf220eb1995ba224669573
Guilherme Ferreira <guifrribeiro@gmail.com>
2020-08-19 17:40:42 -0300

    Commit the venv

---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/run/media/gferreira/Files/Projects/Landix/tecnodix2020/new/backendsecretaria/venv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
